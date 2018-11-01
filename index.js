20fb936e0c2d68aa0addc33b31b9c627dc7bab67
David Jinely <davidjinely@gmail.com>
2018-11-02 00:26:23 +0200

    Fixes bug causing gaps to occur when dragging beyond the end of the list and back

--- index.js
+++ index.js
@@ -299,7 +299,11 @@ class SortableListView extends React.Component {
   }
 
   handleContentSizeChange = (width, height) => {
-    this.scrollContainerHeight = height
+    this.scrollContainerHeight = height;
+
+    if (this.scrollValue > 0) {
+      this.scrollTo({ y: height, animated: !this.props.disableAnimatedScrolling });
+    }
   }
 
   scrollAnimation = () => {
